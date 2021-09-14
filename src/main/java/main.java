4f61ff8a6ec2f2b92a07f6af2fa85e13623be95f
Mschuppers <m.schuppers@belastingdienst.nl>
2021-09-14 13:17:04 +0200

    .

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -12,38 +12,9 @@ public class main {
     public static void main(String[] args) {
         //excersize1();
         //excersize2();
-        excersize3()
+        excersize3();
     }
 
-    private static void excersize2() {
-        System.out.println("Enter season of choice");
-        Scanner sc = new Scanner(System.in);
-
-        String seasonSubmited = sc.next();;
-        switch(seasonSubmited) {
-            case "Zomer":
-                System.out.println(type.WARM);
-                break;
-            case "Lente":
-                System.out.println(type.WARM);
-                break;
-            case "Winter":
-                System.out.println(type.COLD);
-                break;
-            case "Herfst":
-                System.out.println(type.COLD);
-                break;
-            default:
-                System.out.println(type.UNKNOWN);
-
-        }
-
-        public void excersize3(){
-
-        }
-
-
-    }
 
     private static void excersize1() {
         userAddNumber();
@@ -72,7 +43,7 @@ public class main {
         System.out.println("Het totaal is " + totalValue);
     }
 
-    public static void calculateOk() {
+    private static void calculateOk() {
         if (totalValue % 11 == 0) {
             System.out.println("Is ok");
         } else {
@@ -81,4 +52,74 @@ public class main {
 
 
     }
+
+
+    private static void excersize2() {
+
+        System.out.println("Enter season of choice");
+        Scanner sc = new Scanner(System.in);
+        String seasonSubmited = sc.next();
+
+
+        switch (seasonSubmited) {
+            case "Zomer":
+                System.out.println(type.WARM);
+                break;
+            case "Lente":
+                System.out.println(type.WARM);
+                break;
+            case "Winter":
+                System.out.println(type.COLD);
+                break;
+            case "Herfst":
+                System.out.println(type.COLD);
+                break;
+            default:
+                System.out.println(type.UNKNOWN);
+
+        }
+
+
+    }
+
+    private static void excersize3() {
+        drawHourglass();
+
+    }
+
+    public static void drawHourglass() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number ");
+        int n = sc.nextInt();
+
+        int x = n;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j > 0; j--) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < x; k++) {
+                System.out.print("*");
+
+            }
+            System.out.println("");
+            x-=2;
+
+        }
+
+        x = 3; // <-- not 1 here, the first line has 3 asterisks
+        for (int i = 1; i < n; i++) { // <-- i starts at 1 because the first line was already drawn in the upper half
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k < x; k++) {
+                System.out.print("*");
+            }
+            x += 2;
+            System.out.println();
+        }
+
+    }
+
 }
